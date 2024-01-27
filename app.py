de222128e121efff0df4e2441aa95e37f5914b20
juliana1390 <juliana13290@gmail.com>
2024-01-27 17:50:07 -0300

    prompt_add_movie

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 import database
 
 menu = """Please select one of the following options:
@@ -15,9 +16,17 @@ welcome = "Welcome to the watchlist app!"
 print(welcome)
 database.create_table()
 
+def prompt_add_movie():
+    title = input("Movie title: ")
+    release_date = input("Release date (dd-mm-YYYY): ")
+    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+    timestamp = parsed_date.timestamp()
+
+    database.add_movie(title, timestamp)
+
 while (user_input := input(menu)) != "6":
     if user_input == "1":
-        pass
+        prompt_add_movie()
     elif user_input == "2":
         pass
     elif user_input == "3":
