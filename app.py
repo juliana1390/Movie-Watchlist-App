d7f56138252efb13d55f71c961f847775c5c72fe
juliana1390 <juliana13290@gmail.com>
2024-01-27 18:06:30 -0300

    print_movie_list

--- app.py
+++ app.py
@@ -24,11 +24,18 @@ def prompt_add_movie():
 
     database.add_movie(title, timestamp)
 
+def print_movie_list():
+    print("-- Upcoming movies --")
+    for movie in movies:
+        print(f"{movie[0]} (on {movie[1]})")
+    print("---- \n")
+
 while (user_input := input(menu)) != "6":
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
-        pass
+        movies = database.get_movies(True)
+        print_movie_list(movies)
     elif user_input == "3":
         pass
     elif user_input == "4":
